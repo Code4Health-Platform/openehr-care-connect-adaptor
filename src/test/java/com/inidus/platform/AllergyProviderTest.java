d4c0cfb00321e2c4e2b4d4839c424edcfe92e50d
lars <lars@inidus.com>
2017-12-21 12:43:32 +0000

    chore: Rename

--- src/test/java/com/inidus/platform/AllergyProviderTest.java
+++ src/test/java/com/inidus/platform/AllergyProviderTest.java
@@ -24,7 +24,7 @@ public class AllergyProviderTest {
     @Qualifier("AllergyProvider")
     private AllergyProvider testProvider;
     @Autowired
-    private OpenEhrConnector ehrService;
+    private OpenEhrConnector ehrConnector;
 
     @Before
     public void setUp() throws Exception {
@@ -32,20 +32,13 @@ public class AllergyProviderTest {
 
     @Test
     public void getAllResources() throws Exception {
-        configureEthercisCdr("http://178.62.71.220:8080", "guest", "guest", true);
+        configureCdrConnector("http://178.62.71.220:8080", "guest", "guest", true);
         Assert.assertNotNull(testProvider.getAllResources());
-    }
-
-    private void configureEthercisCdr(String url, String user, String pass, boolean isToken) {
-        ehrService.setIsTokenAuth(isToken);
-        ehrService.setUrl(url);
-        ehrService.setUsername(user);
-        ehrService.setPassword(pass);
     }
 
     @Test
     public void getResourceByPatientIdentifier_ehrNamespace() throws Exception {
-        configureEthercisCdr("http://178.62.71.220:8080", "guest", "guest", true);
+        configureCdrConnector("http://178.62.71.220:8080", "guest", "guest", true);
         TokenParam identifier = new TokenParam("uk.nhs.nhs_number", "9999999000");
         List<CCAllergyIntolerance> result = testProvider.getFilteredResources(identifier, null, null);
         Assert.assertNotNull(result);
@@ -54,7 +47,7 @@ public class AllergyProviderTest {
 
     @Test
     public void getResourceByPatientIdentifier_FhirNamespace() throws Exception {
-        configureEthercisCdr("http://178.62.71.220:8080", "guest", "guest", true);
+        configureCdrConnector("http://178.62.71.220:8080", "guest", "guest", true);
         TokenParam identifier = new TokenParam("https://fhir.nhs.uk/Id/nhs-number", "9999999000");
         List<CCAllergyIntolerance> result = testProvider.getFilteredResources(identifier, null, null);
         Assert.assertNotNull(result);
@@ -63,7 +56,7 @@ public class AllergyProviderTest {
 
     @Test
     public void getResourceByDate() throws Exception {
-        configureEthercisCdr("https://test.operon.systems", "oprn_hcbox", "XioTAJoO479", true);
+        configureCdrConnector("https://test.operon.systems", "oprn_hcbox", "XioTAJoO479", true);
 
         Date from = DatatypeConverter.parseDateTime("2016-12-07T15:47:43+01:00").getTime();
         Date to = DatatypeConverter.parseDateTime("2018-12-07T15:47:43+01:00").getTime();
@@ -76,7 +69,7 @@ public class AllergyProviderTest {
 
     @Test
     public void getResourceByDate_withouth_from() throws Exception {
-        configureEthercisCdr("https://test.operon.systems", "oprn_hcbox", "XioTAJoO479", true);
+        configureCdrConnector("https://test.operon.systems", "oprn_hcbox", "XioTAJoO479", true);
 
         Date to = DatatypeConverter.parseDateTime("2018-12-07T15:47:43+01:00").getTime();
         DateRangeParam dateRange = new DateRangeParam(null, to);
@@ -88,7 +81,7 @@ public class AllergyProviderTest {
 
     @Test
     public void getResourceByDate_withouth_to() throws Exception {
-        configureEthercisCdr("https://test.operon.systems", "oprn_hcbox", "XioTAJoO479", true);
+        configureCdrConnector("https://test.operon.systems", "oprn_hcbox", "XioTAJoO479", true);
 
         Date from = DatatypeConverter.parseDateTime("2016-12-07T15:47:43+01:00").getTime();
         DateRangeParam dateRange = new DateRangeParam(from, null);
@@ -100,11 +93,18 @@ public class AllergyProviderTest {
 
     @Test
     public void getResourceByCategory_medication() throws Exception {
-        configureEthercisCdr("http://178.62.71.220:8080", "guest", "guest", true);
+        configureCdrConnector("http://178.62.71.220:8080", "guest", "guest", true);
         StringParam food = new StringParam("medication");
 
         List<CCAllergyIntolerance> result = testProvider.getFilteredResources(null, food, null);
 
         Assert.assertNotNull(result);
+    }
+
+    private void configureCdrConnector(String url, String user, String pass, boolean isToken) {
+        ehrConnector.setIsTokenAuth(isToken);
+        ehrConnector.setUrl(url);
+        ehrConnector.setUsername(user);
+        ehrConnector.setPassword(pass);
     }
 }
